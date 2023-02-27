a09a197aeb2ad550a134151d1031fc3d5e715201
Dan Goodman <dan@danthegoodman.com>
2023-02-27 10:25:26 -0500

    ready for testing

---
+++ segmentFunction.js
@@ -0,0 +1,75 @@
+// Learn more about destination functions API at
+// https://segment.com/docs/connections/destinations/destination-functions
+
+/**
+ * Handle track event
+ * @param  {SegmentTrackEvent} event
+ * @param  {FunctionSettings} settings
+ */
+async function onTrack(event, settings) {
+	const endpoint = `https://api.us-east.tinybird.co/v0/events?name=${encodeURIComponent(
+		settings.tableName
+	)}`;
+	let response;
+
+	try {
+		console.log('sending data', JSON.stringify(event, null, 2));
+		response = await fetch(endpoint, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${settings.token}`
+			},
+			body: JSON.stringify({
+				...event,
+				properties: JSON.stringify(event.properties),
+				context: JSON.stringify(event.context),
+				traits: JSON.stringify(event.traits)
+			})
+		});
+		console.log('sent');
+	} catch (error) {
+		// Retry on connection error
+		throw new RetryError(error.message);
+	}
+
+	if (response.status >= 500 || response.status === 429) {
+		// Retry on 5xx (server errors) and 429s (rate limits)
+		throw new RetryError(`Failed with ${response.status}`);
+	}
+}
+
+async function onBatch(events, settings) {
+	const endpoint = `https://api.us-east.tinybird.co/v0/events?name=${encodeURIComponent(
+		settings.tableName
+	)}`;
+	let response;
+
+	try {
+		console.log('sending data', JSON.stringify(events, null, 2));
+		response = await fetch(endpoint, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${settings.token}`
+			},
+			body: events
+				.map(event =>
+					JSON.stringify({
+						...event,
+						properties: JSON.stringify(event.properties),
+						context: JSON.stringify(event.context),
+						traits: JSON.stringify(event.traits)
+					})
+				)
+				.join('\n') // ndjson
+		});
+		console.log('sent');
+	} catch (error) {
+		// Retry on connection error
+		throw new RetryError(error.message);
+	}
+
+	if (response.status >= 500 || response.status === 429) {
+		// Retry on 5xx (server errors) and 429s (rate limits)
+		throw new RetryError(`Failed with ${response.status}`);
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export interface Env {}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const res = new Response("writing in background");
+		ctx.waitUntil(async function(): Promise<void> {
+			const response = await fetch(`https://api.us-east.tinybird.co/v0/events?name=${encodeURIComponent(
+				new URL(request.url).searchParams.get("name") as string
+			)}`, {
+				method: request.method,
+				headers: request.headers,
+				body: request.body
+			})
+			console.log("Response from tinybird", response.status, await response.text())
+		}())
+		return res
+	},
+};
